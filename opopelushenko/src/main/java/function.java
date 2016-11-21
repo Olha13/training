5f1d7fea35f937e7d2fdfd85856f7c3338450a92
Olga Popelyushenko <opop@kyiv.ciklum.net>
2016-11-21 11:12:35 +0200

    Second commit

---
+++ opopelushenko/src/main/java/function.java
@@ -0,0 +1,9 @@
+
+public class function {
+
+    public int getSum(int a, int b){
+        int c = a + b;
+        return c;
+    }
+
+}
---
+++ opopelushenko/src/test/java/I_test.java
@@ -0,0 +1,7 @@
+import org.junit.*;
+
+public class I_test {
+    public static void main(String[] args) {
+        System.out.println("la la la");
+    }
+}
